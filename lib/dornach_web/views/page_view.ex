052fdd8bd99e223fe3a21483b3f36531550ea1aa
Mario Uher <uher.mario@gmail.com>
2019-12-28 18:09:46 +0100

    Move some tests into doctests

--- lib/dornach_web/views/page_view.ex
+++ lib/dornach_web/views/page_view.ex
@@ -1,16 +1,60 @@
 defmodule DornachWeb.PageView do
   use DornachWeb, :view
 
+  @doc """
+  Returns the first day for a calendar. As this calendar always starts with a
+  Monday, this day could be in the previous month of the given date.
+
+  ## Examples
+
+      iex> first_day_of_calendar(~D[2019-11-01])
+      ~D[2019-10-28]
+
+      iex> first_day_of_calendar(~D[2019-12-01])
+      ~D[2019-11-25]
+
+      iex> first_day_of_calendar(~D[2020-01-01])
+      ~D[2019-12-30]
+
+  """
   def first_day_of_calendar(date \\ Date.utc_today()) do
     first = %{date | day: 1}
     Date.add(first, Date.day_of_week(first) * -1 + 1)
   end
 
+  @doc """
+  Returns the last day for a calendar. As this calendar always ends with a
+  Sunday, this day could be in the next month of the given date.
+
+  ## Examples
+
+      iex> last_day_of_calendar(~D[2019-11-01])
+      ~D[2019-12-01]
+
+      iex> last_day_of_calendar(~D[2019-12-01])
+      ~D[2020-01-05]
+
+      iex> last_day_of_calendar(~D[2020-01-01])
+      ~D[2020-02-02]
+
+  """
   def last_day_of_calendar(date \\ Date.utc_today()) do
     last = %{date | day: Date.days_in_month(date)}
     Date.add(last, 7 - Date.day_of_week(last))
   end
 
+  @doc """
+  Returns the first day of the previous month for the given date.
+
+  ## Examples
+
+      iex> first_day_of_previous_month(~D[2019-11-01])
+      ~D[2019-10-01]
+
+      iex> first_day_of_previous_month(~D[2019-01-01])
+      ~D[2018-12-01]
+
+  """
   def first_day_of_previous_month(date) do
     {year, month, _} = Date.to_erl(date)
     max = Date.months_in_year(date)
@@ -24,6 +68,18 @@ defmodule DornachWeb.PageView do
     date
   end
 
+  @doc """
+  Returns the first day of the next month for the given date.
+
+  ## Examples
+
+    iex> first_day_of_next_month(~D[2019-11-01])
+    ~D[2019-12-01]
+
+    iex> first_day_of_next_month(~D[2019-12-01])
+    ~D[2020-01-01]
+
+  """
   def first_day_of_next_month(date) do
     {year, month, _} = Date.to_erl(date)
     max = Date.months_in_year(date)
