28b008940af0327ba64f4dd63e0d91d30c05d8f8
Mario Uher <uher.mario@gmail.com>
2020-01-06 18:46:12 +0100

    Get sockets and live views working

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dornach, DornachWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "jw-dornach.at", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- config/releases.exs
+++ config/releases.exs
@@ -3,4 +3,4 @@ import Config
 config :dornach, DornachWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  url: [host: "jw-dornach.at", port: 443]
